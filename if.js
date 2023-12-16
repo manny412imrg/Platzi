401bcbcb001e2b628defb6a33cb293de30a95a83
Ivan Romero Gutierrez <manny.parte.2@gmail.com>
2023-12-15 22:17:44 -0400

    else if

--- if.js
+++ if.js
@@ -1,3 +1,15 @@
 if(true){
     console.log("hola " )
+} else {
+    console.log("falso " )
+}
+
+var edad = 18;
+
+if (edad === 18){
+    console.log("puedes votar, sera tu primera votacion");
+} else if (edad > 18) {
+    console.log("puedes votar, denuevo");
+} else {
+    console.log(" No puedes votar")
 }
